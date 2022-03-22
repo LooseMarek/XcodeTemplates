4212fcd4f74ea87defbf41fdfb42db7a860da65d
Marek Loose <loose.marek@gmail.com>
2022-03-22 15:21:58 +0100

    Added example File template

---
+++ Swift UI VIP Scene.xctemplate/___FILEBASENAME___Router.swift
@@ -0,0 +1,16 @@
+//___FILEHEADER___
+
+import SwiftUI
+import Presentation
+
+public protocol ___VARIABLE_productName___RouterProtocol: RouterProtocol {
+}
+
+public class ___VARIABLE_productName___Router: ___VARIABLE_productName___RouterProtocol {
+
+    public init() {}
+
+    public func start() -> AnyView {
+        return AnyView(___VARIABLE_productName___View())
+    }
+}
---
+++ Swift UI VIP Scene.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,17 @@
+//___FILEHEADER___
+
+import SwiftUI
+
+struct ___VARIABLE_productName___View: View {
+
+    var body: some View {
+        Text("Hello, ___VARIABLE_productName___ View!")
+            .padding()
+    }
+}
+
+struct ___VARIABLE_productName___View_Previews: PreviewProvider {
+    static var previews: some View {
+        ___VARIABLE_productName___View()
+    }
+}
